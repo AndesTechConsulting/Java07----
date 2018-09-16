455b6dd2ea896884c02814eee884b702f2757563
Egor_Vlasov <crazy13.92@inbox.ru>
2018-09-16 21:43:38 +0600

    add sqlite

---
+++ src/main/java/org/andestech/learning/rfb18/Customer.java
@@ -0,0 +1,64 @@
+package org.andestech.learning.rfb18;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private String sname;
+    private double salary;
+    private int age;
+    private String creditRating;
+
+
+    public void getFromDB(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCreditRating() {
+        return creditRating;
+    }
+
+    public void setCreditRating(String creditRating) {
+        this.creditRating = creditRating;
+    }
+}
